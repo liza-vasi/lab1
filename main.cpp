0ee87fbb1112e83825faa1a6201ace5deb2756c1
liza_vasi <viki.sova@yandex.ru>
2026-05-06 03:19:22 +0700

    Added a function to exit

--- main.cpp
+++ main.cpp
@@ -35,26 +35,33 @@ int main(int argc, char *argv[])
     FileManager& manager = FileManager::instance();
     Logger& logger = Logger::instance();
 
-    QObject::connect(&manager, &FileManager::fileCreate,&logger, &Logger::onFileCreated);
-    QObject::connect(&manager, &FileManager::fileDelete,&logger, &Logger::onFileDeleted);
-    QObject::connect(&manager, &FileManager::fileChangeSize,&logger, &Logger::onFileSizeChanged);
-    QObject::connect(&manager, &FileManager::fileModif,&logger, &Logger::onFileModified);
-    QObject::connect(&manager, &FileManager::fileChange,&logger, &Logger::onFileChanged);
+    QObject::connect(&manager, &FileManager::fileCreate, &logger, &Logger::onFileCreated);
+    QObject::connect(&manager, &FileManager::fileDelete, &logger, &Logger::onFileDeleted);
+    QObject::connect(&manager, &FileManager::fileChangeSize, &logger, &Logger::onFileSizeChanged);
+    QObject::connect(&manager, &FileManager::fileModif, &logger, &Logger::onFileModified);
+    QObject::connect(&manager, &FileManager::fileChange, &logger, &Logger::onFileChanged);
 
     logger.log("File watcher");
     logger.log("Check interval: " + QString::number(intervalMs) + " ms");
     logger.log("Watched files:");
-
     for (const QString &path : filePaths) {
         logger.log("  " + path);
     }
     logger.log("Initial state:");
 
     manager.addFiles(filePaths);
-
     while (running) {
         manager.checkAllFiles();
+
+
+        std::string command;
+        std::getline(std::cin, command);
+
+        if (command == "exit") {
+            running = false;
+        }
         std::this_thread::sleep_for(std::chrono::milliseconds(intervalMs));
     }
+    logger.log("Program terminated");
     return 0;
 }
